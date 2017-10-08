06dec2e62a98664e3464e09b187c1d86d52091d8
Sudono Tanjung <sudtanj@users.noreply.github.com>
2017-10-08 07:42:45 +0700

    Add files via upload

---
+++ form.js
@@ -0,0 +1,6 @@
+function cssubmit(){
+var name=document.getElementsByName("name")[0].value;
+var email=document.getElementsByName("email")[0].value;
+var message=document.getElementsByName("message")[0].value
+ alert("Name :"+name+"\n"+"Email :"+email+"\n Message :"+message);
+}
